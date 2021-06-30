b5066c939c371d623308c9eba261f395c6ce0672
Harri Evans <hevans9000@gmail.com>
2021-06-30 11:07:22 +0100

    build: migrations to nx 12

--- apps/harri-io/jest.config.js
+++ apps/harri-io/jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  name: "fun-app",
-  preset: "../../jest.config.js",
-  coverageDirectory: "../../coverage/apps/harri-io/"
+  preset: '../../jest.preset.js',
+  coverageDirectory: '../../coverage/apps/harri-io/',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
+  displayName: 'harri-io',
 };
